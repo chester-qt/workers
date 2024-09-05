1beb53bc9eb19f74f89c1030bb31b9ffccdf5c30
chester-qt <chester.jazmin@qualitytrade.com>
2024-09-05 13:21:19 +0800

    add comments

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import worker as w
 
 class main:
     
+    # Enter path to directory of your qualitytrade projects
     path = "/qt/iaf"
     
+    # Repository name
     repo = "iaf-dc-backend"
     
+    # Branch name
     branch = "develop"
     
+    # Add or remove queues here
     queues = [
         "iaf-os-dev",
         "iaf-upload-dev",
@@ -20,8 +24,10 @@ class main:
         "iaf-mdb-publishing-dev"
     ]
     
+    # Memory in MB
     memory = 1024
     
+    # Name of terminal (defaults to iTerm)
     terminal = "iTerm"
     
     process_workers = w.worker(queues, memory, path, repo, branch, terminal)
